80b6b665e1878dd632b89af431ef238866553e02
Akanoa <noa@guern.eu>
2014-07-11 15:46:24 +0200

    soutenance

--- soutenance/src/scripts/main.js
+++ soutenance/src/scripts/main.js
@@ -8,3 +8,22 @@ bespoke.from('article', {
   state: true,
   forms: true
 });
+
+//hide or display screen when b key is pressed
+$("body").keypress(function(e) {
+  console.log(e.keyCode);
+  if(e.keyCode==66 || e.keyCode==98){
+    $(".black-hide").fadeToggle('slow/400/fast', function() {
+      
+    });
+  }
+});
+
+$("body").on('click', function(e) { 
+   if( e.which == 2 ) {
+      e.preventDefault();
+      $(".black-hide").fadeToggle('slow/400/fast', function() {
+      
+      });
+   }
+});
